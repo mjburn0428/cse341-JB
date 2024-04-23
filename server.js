ad00397002da910c8776d11742e362baa91d671b
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-04-22 21:16:50 -0600

    Adjusted server port number

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(3000, () => {
-    console.log('Sevrer is running on port ${port}');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
