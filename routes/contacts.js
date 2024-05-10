59e14bea369553052bbd2710cb9f1c4391f66e8e
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-05-09 21:59:25 -0600

    Week03 Update

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
-
+//lesson2
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+//lesson3
 router.post('/', contactsController.createContact);
 
+router.put('/:id', contactsController.updateContact);
 
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const myController = require('../controllers');
 
+
+//lesson1
+router.get('/', myController.awesomeFunction);
+router.get('/awesome', myController.returnanotherAnimal);
+router.get('/awesome2', myController.returnanotherAnimal2);
+
+//lesson2
 router.use('/contacts', require('./contacts'))
 
 
