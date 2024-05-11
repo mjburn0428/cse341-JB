55c7fdd8fa54bb870d115d06fd0d65bbbdbeeceb
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-05-10 20:14:47 -0600

    Update for week3

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -62,7 +62,7 @@ const updateContact = async (req, res) => {
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb.getDb().db().collection('contacts').delete({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
@@ -70,6 +70,7 @@ const deleteContact = async (req, res) => {
     res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
   }
 };
+
 
 
 module.exports = { getAll, getSingle, createContact, updateContact, deleteContact};
