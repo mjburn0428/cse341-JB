446afe98bc21ba65535398bb86a5429e0d22d58e
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-05-18 11:56:16 -0600

    Fixed error (hopefully)

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     res.setHeader('Content-Type', 'application/json');
