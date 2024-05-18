76989ca900c21dfa5cc29fa93816676a46e3d0fe
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-05-18 11:16:27 -0600

    Updated app.js for Frontend

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://cse341-contacts-frontend.netlify.app/');
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'Origin, X-Requested-with, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
+    
   })
   .use('/', require('./routes'));
 
