cffbb8892bdbc0dae944c783ac0eaaa17462d0f4
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-05-18 12:04:55 -0600

    Testing cors

--- app.js
+++ app.js
@@ -2,24 +2,13 @@ require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
+app.use(bodyParser.json()).use(cors()).use('/', require('./routes'));
 
-  })
-  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
