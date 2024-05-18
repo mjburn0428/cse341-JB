ab0005abf5835827d0cf27ea73370ce6cb718a7e
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-05-18 11:23:53 -0600

    fixed

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Origin',
-      'Origin, X-Requested-with, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    //res.setHeader(
+      //'Access-Control-Allow-Origin',
+      //'Origin, X-Requested-with, Content-Type, Accept, Z-Key'
+    //);
+    //res.setHeader('Content-Type', 'application/json');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 
   })
